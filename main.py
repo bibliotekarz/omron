b943fc1a03546d8f5560fa3bb8e2c528e0992d08
bibliotekarz <arek@bibliotekarz.com>
2020-09-01 19:05:00 +0200

    bardzo_stare nie działa

--- main.py
+++ main.py
@@ -24,6 +24,18 @@ def robi_stare(data_pomiaru, wysokie, niskie, tetno):
     stare_tetno = tetno
     return stara_data, stare_wysokie, stare_niskie, stare_tetno
 
+bardzo_stare_niskie = 1110
+bardzo_stare_tetno = 1110
+bardzo_stara_data = 1110
+bardzo_stare_wysokie = 1110
+
+def robi_bardzo_stare(stara_data, stare_wysokie, stare_niskie, stare_tetno):
+    bardzo_stara_data = stara_data
+    bardzo_stare_wysokie = stare_wysokie
+    bardzo_stare_niskie = stare_niskie
+    bardzo_stare_tetno = stare_tetno
+    return bardzo_stara_data, bardzo_stare_wysokie, bardzo_stare_niskie, bardzo_stare_tetno
+
 def popraw_date(dane, pobrana_data):
 
     data = pobrana_data.split(".")
@@ -44,12 +56,18 @@ def popraw_godzine(pobrana_godzina):
     return godzina
 
 licznik = 1
-def wylicz_srednia(stara_data, nowa_data, stary_parametr, nowy_parametr, licznik):
+bardzo_stary_parametr = 0
+def wylicz_srednia(bardzo_stary_parametr, stara_data, nowa_data, stary_parametr, nowy_parametr, licznik):
     licznik0 = licznik
     if stara_data == nowa_data:
         licznik0 += 1
-        parametr = (int(stary_parametr) + int(nowy_parametr)) / licznik0
-#        print("--licznik0 stara data = nowa data", licznik0)
+        if licznik0 < 3:
+            parametr = (int(stary_parametr) + int(nowy_parametr)) / licznik0
+#            print("średnia ", licznik0," pomiarów ", parametr)
+        elif licznik0 > 2:
+            parametr = (int(bardzo_stary_parametr) + int(stary_parametr) + int(nowy_parametr)) / licznik0
+#            print("średnia ", licznik0," pomiarów ", parametr)
+
         licznik = licznik0
 
 #        print("--licznik stara data = nowa data", licznik)
@@ -62,17 +80,19 @@ def wylicz_srednia(stara_data, nowa_data, stary_parametr, nowy_parametr, licznik
 
 # :TODO: zrobić średnią niezależna od liczby pomiarów
 # f = open('csv/as.csv','r')
-f = open('csv/_as BP Report 012220.csv','r')
+f = open('csv/as.csv','r')
 wszystkie_dane = f.read()
 pobrane_dane = wszystkie_dane.split("\n")
 poprawiona_data = 0
 wysokie = 0
 niskie = 0
 tetno = 0
+bardzo_stare = (123, 123, 123, 123)
 
 for dane in pobrane_dane:
 
     stare = robi_stare(poprawiona_data, wysokie, niskie, tetno)
+    print(stare, "stare")
 
     if dane == "":
         print("awaria puste")
@@ -93,14 +113,22 @@ for dane in pobrane_dane:
 
     nowe = (poprawiona_data, wysokie, niskie, tetno)
     print("-----")
-    wys = wylicz_srednia(stare[0], nowe[0], stare[1], nowe[1], licznik)[0]
-    nis = wylicz_srednia(stare[0], nowe[0], stare[2], nowe[2], licznik)[0]
-    tet = wylicz_srednia(stare[0], nowe[0], stare[3], nowe[3], licznik)[0]
+    print(nowe, "nowe")
+
+    print(stare, "stare")
+
+    wys = wylicz_srednia(bardzo_stare[1], stare[0], nowe[0], stare[1], nowe[1], licznik)[0]
+    nis = wylicz_srednia(bardzo_stare[2], stare[0], nowe[0], stare[2], nowe[2], licznik)[0]
+    tet = wylicz_srednia(bardzo_stare[3], stare[0], nowe[0], stare[3], nowe[3], licznik)[0]
 
     print(nowe[0], "wysokie ", int(wys), "s"+str(stare[1]), "n"+str(nowe[1]))
     print(nowe[0], "niskie ", int(nis), "s"+str(stare[2]), "n"+str(nowe[2]))
     print(nowe[0], "tetno ", int(tet), "s"+str(stare[3]), "n"+str(nowe[3]))
 
 #    print("co zwraca", wylicz_srednia(stare[0], nowe[0], stare[1], nowe[1], licznik))
-    licznik = wylicz_srednia(stare[0], nowe[0], stare[1], nowe[1], licznik)[1]
+    licznik = wylicz_srednia(bardzo_stare[1], stare[0], nowe[0], stare[1], nowe[1], licznik)[1]
     print("ostatni licznik", licznik)
+
+
+    bardzo_stare = robi_bardzo_stare(stare[0], stare[1], stare[2], stare[3])
+    print(bardzo_stare, "bardzo_stare koniec")
